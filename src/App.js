2ae9b9f5c90da527a274ffd96b6fa13de53bf1cc
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-08 23:14:19 +0600

    basic route setup test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Router/Routes/Routes";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center hover:bg-blue-300 hover:cursor-pointer">
-        Hello world!
-      </h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
