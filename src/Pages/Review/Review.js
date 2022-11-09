7197aa0f3e77310a10708c6e7ff2315ed900618f
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-09 21:41:18 +0600

    review page updated

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AllReview from "./AllReview";
 
 const Review = () => {
@@ -6,12 +7,17 @@ const Review = () => {
     <div className="mb-20">
       <div className="text-center mb-10">
         <h1 className="text-5xl font-bold mb-3">Reviews</h1>
-        <h1>This is Review Page</h1>
+        <h1>
+          This is Review Page where you can see all reviews and you can also
+          write a review
+        </h1>
       </div>
       <div className="w-2/4 mx-auto text-center">
-        <button className="lg:text-lg btn btn-warning ">
-          Please login to write a review
-        </button>
+        <Link to="/login">
+          <button className="lg:text-lg btn btn-warning hover:text-white">
+            Please login to post a review
+          </button>
+        </Link>
       </div>
       <AllReview />
     </div>
