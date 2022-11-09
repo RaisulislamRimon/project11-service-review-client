8c360a78025c1f51a672d88b2c05abeb2098d0ba
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-09 21:29:47 +0600

    setup private route

---
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import { TailSpin } from "react-loader-spinner";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-96">
+        <TailSpin
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  }
+  return children;
+};
+
+export default PrivateRoute;
